b973eba9c14de4f16416c07520c5f8de029159f7
Lucian Ion <lcion@hotmail.com>
2013-04-25 22:13:22 +0100

    cosmetic changes

--- SndAndrCli/gen/com/lion/rmtsndcli/R.java
+++ SndAndrCli/gen/com/lion/rmtsndcli/R.java
@@ -45,14 +45,15 @@ public final class R {
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
-        public static final int alexBtn=0x7f050004;
-        public static final int andreiBtn=0x7f050005;
+        public static final int alexBtn=0x7f050005;
+        public static final int andreiBtn=0x7f050006;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
-        public static final int luciBtn=0x7f050003;
-        public static final int mute=0x7f050008;
-        public static final int snd20Btn=0x7f050007;
-        public static final int title_activity_client=0x7f050006;
+        public static final int connecting=0x7f050003;
+        public static final int luciBtn=0x7f050004;
+        public static final int mute=0x7f050009;
+        public static final int snd20Btn=0x7f050008;
+        public static final int title_activity_client=0x7f050007;
+        public static final int top_main_string=0x7f050002;
     }
     public static final class style {
         /** 

ff5d9b756e1f137a5ca7681d454b4ba3f1245224
Lucian Ion <lcion@hotmail.com>
2013-04-28 21:29:02 +0100

    use list to show pc
    add list of computers in a list view
    add computer to the list view
    remove computer from the list view
    input dialog for name and ip

--- SndAndrCli/gen/com/lion/rmtsndcli/R.java
+++ SndAndrCli/gen/com/lion/rmtsndcli/R.java
@@ -24,11 +24,10 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080009;
+        public static final int action_settings=0x7f080008;
         public static final int button1=0x7f080006;
-        public static final int button2=0x7f080007;
-        public static final int button3=0x7f080008;
         public static final int checkBoxMute=0x7f080002;
+        public static final int listView1=0x7f080007;
         public static final int seekBarVol=0x7f080003;
         public static final int send20=0x7f080001;
         public static final int textView1=0x7f080005;
@@ -49,8 +48,9 @@ public final class R {
         public static final int andreiBtn=0x7f050006;
         public static final int app_name=0x7f050000;
         public static final int connecting=0x7f050003;
-        public static final int luciBtn=0x7f050004;
+        public static final int contentDescription=0x7f05000a;
         public static final int mute=0x7f050009;
+        public static final int onAddNewPCBtn=0x7f050004;
         public static final int snd20Btn=0x7f050008;
         public static final int title_activity_client=0x7f050007;
         public static final int top_main_string=0x7f050002;
--- SndAndrCli/src/com/lion/rmtsndcli/MainActivity.java
+++ SndAndrCli/src/com/lion/rmtsndcli/MainActivity.java
@@ -1,51 +1,153 @@
 package com.lion.rmtsndcli;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
 
 	public final static String EXTRA_MESSAGE = "com.lion.rmtsndcli.MESSAGE";
-	
+	private ListView listView;
+	private ArrayList<String> listNames;
+	private ArrayList<String> listIps;
+	private StableArrayAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+		listView = (ListView)findViewById(R.id.listView1);
+		String[] namesValues = new String[] { "Luci PC", "Alex PC", "Andrei PC"};
+		String[] ipValues = new String[] { "192.168.1.12", "192.168.1.13", "192.168.1.11"};
+		listNames = new ArrayList<String>();
+	    for (int i = 0; i < namesValues.length; ++i) {
+	      listNames.add(namesValues[i]);
+	    }
+	    listIps = new ArrayList<String>();
+	    for (int i = 0; i < ipValues.length; ++i) {
+	    	listIps.add(ipValues[i]);
+		    }
+	    
+	    adapter = new StableArrayAdapter(this,
+	        android.R.layout.simple_list_item_1, listNames);
+	    listView.setAdapter(adapter);
+
+	    listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+	      @Override
+	      public void onItemClick(AdapterView<?> parent, final View view,
+	          int position, long id) {
+	        String message = listIps.get(position);
+	        startClientActivity(message);
+	      }
+	    });
+	    listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+
+			@Override
+			public boolean onItemLongClick(AdapterView<?> parent, View view,
+					int position, long id) {
+		        String item = (String) parent.getItemAtPosition(position);
+		    	listNames.remove(item);
+				listIps.remove(position);
+		    	adapter.updateMap(listNames);
+	            adapter.notifyDataSetChanged();
+				return true;
+			}
+		});
     }
 
+    private class StableArrayAdapter extends ArrayAdapter<String> {
+
+        HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
+
+        public StableArrayAdapter(Context context, int textViewResourceId,
+            List<String> objects) {
+          super(context, textViewResourceId, objects);
+          for (int i = 0; i < objects.size(); ++i) {
+            mIdMap.put(objects.get(i), i);
+          }
+        }
+
+        @Override
+        public long getItemId(int position) {
+          String item = getItem(position);
+          return mIdMap.get(item);
+        }
+
+        public void updateMap(List<String> objects){
+        	mIdMap.clear();
+            for (int i = 0; i < objects.size(); ++i) {
+                mIdMap.put(objects.get(i), i);
+              }
+        }
+
+        @Override
+        public boolean hasStableIds() {
+          return true;
+        }
+
+      }
     /** Called when the user clicks the Send button */
-    public void onLuciBtn(View view) {
+    public void onAddNewPCBtn(View view) {
+    	getNameIpFromUser();
+    }
+    
+    private int getNameIpFromUser(){
+    	AlertDialog.Builder alert = new AlertDialog.Builder(this);
+
+    	alert.setTitle("Add new PC");
+    	alert.setMessage("IP Address:");
+
+    	// Set an EditText view to get user input 
+    	final EditText input = new EditText(this);
+    	alert.setView(input);
+
+    	alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+    	public void onClick(DialogInterface dialog, int whichButton) {
+    		String value = input.getText().toString();
+    	  	// Do something with value!
+	      	listNames.add(value);
+	      	listIps.add(value);
+	      	adapter.updateMap(listNames);
+	      	adapter.notifyDataSetChanged();
+
+    	  }
+    	});
+
+    	alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+    	  public void onClick(DialogInterface dialog, int whichButton) {
+    	    // Canceled.
+    	  }
+    	});
+
+    	alert.show();
+    	return 0;
+    }
+    
+    public void startClientActivity(String message) {
         // create new activity
     	Intent intent = new Intent(this, ClientActivity.class);
-    	String message = "192.168.1.12";
     	intent.putExtra(EXTRA_MESSAGE, message);
     	startActivity(intent);
     }
-    
-    public void onAlexBtn(View view) {
-        // create new activity
-    	Intent intent = new Intent(this, ClientActivity.class);
-    	String message = "192.168.1.13";
-    	intent.putExtra(EXTRA_MESSAGE, message);
-    	startActivity(intent);
-    }
-    
-    public void onAndreiBtn(View view) {
-        // create new activity
-    	Intent intent = new Intent(this, ClientActivity.class);
-    	String message = "192.168.1.11";
-    	intent.putExtra(EXTRA_MESSAGE, message);
-    	startActivity(intent);
-    }
-    
+        
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-    
 }

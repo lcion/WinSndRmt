5c384535c499f81ecbfd5f4ac890e260df5c46db
Lucian Ion <lcion@hotmail.com>
2013-09-16 19:31:54 +0100

    update batch file to automatically sign packages

--- Setup/makesetup.bat
+++ Setup/makesetup.bat
@@ -1,7 +1,9 @@
-set path=%path%;c:\Program Files (x86)\WiX Toolset v3.7\bin
+set path=%path%;c:\Program Files (x86)\WiX Toolset v3.7\bin;c:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A\Bin
 copy ..\bin\Release\*.exe .
+signtool sign /f ..\..\keys\exesigning\LucianIonSPC.pfx /p xxxxxx /t http://timestamp.verisign.com/scripts/timstamp.dll *.exe
 candle WinSndRmt.wxs
 light WinSndRmt.wixobj
 del *.exe *.wixpdb *.wixobj
+signtool sign /f ..\..\keys\exesigning\LucianIonSPC.pfx /p xxxxxx /t http://timestamp.verisign.com/scripts/timstamp.dll *.msi
 move WinSndRmt.msi ..\bin\Release\
 pause
